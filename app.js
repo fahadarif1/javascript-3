345d8a128d5288eeb41453e1b882936971c55c37
fahadarif1 <134074010+fahadarif1@users.noreply.github.com>
2024-03-08 11:04:30 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// function sum(firstname,lastname){
+//     var odd = prompt (1,3,5,7,9)
+//     var even = prompt (2,4,6,8,0)
+//     console.log(firstname + "" + lastname)
+ 
+//  }
+
+
+//  var num = 1122
+// if(9 < num < 99){
+//     console.log(two digit number);
+
+// }else if(99 < num < 999){
+//     console.log(three digit number);
+
+// }else if (999 < num < 9999){
+//     console.log(four digitnumber);
+// }else{
+//     console.log(false);
+// }
+
+
+// var driving_age  = 16 
+// var smoking_age  = 18
+// var lotto_age = 19
+// var drinking_age =  21 
+
+
+// if(driving_age >== 0 && drinking_age){
+// console.log('you can drive, smoke, gamble, and drink! woohoon');
+// }else if (age){
+
+// }
+
+
+// var time = prompt("enter your time");
+// if(time < 12){
+//     console.log("am");
+// }
+// else if(time < 24){
+//     console.log("PM");
+// }
+
+
+// var result = prompt("Enter your email")
